37ae16b4c1f2ad17ce69eae36f7e298e5e39ccd3
DRojasV397 <diegorv3971@gmail.com>
2024-06-12 18:44:52 -0600

    5

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def login():
             flash(f'Error iniciando sesión: {str(e)}', 'danger')
     return render_template('login.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
