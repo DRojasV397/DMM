a24b7000815f2368b59f86cc4a0dc9e614a0beea
DiegoRV397 <diegorv3971@gmail.com>
2024-06-13 21:39:11 -0600

    9.1

--- app.py
+++ app.py
@@ -449,5 +449,5 @@ def comprobarLugarVisitado():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     # app.run(debug=True)
